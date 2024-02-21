ee6af7df9c7ba34b47ee29ff8ffbbb7f8b246019
PuneetJhurani <puneet.jhurani@nagarro.com>
2024-02-21 18:43:49 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nagarro on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
